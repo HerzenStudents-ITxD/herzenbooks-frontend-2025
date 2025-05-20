3218161b2942b2e7adcd1e5655d129ffb2c8d424
deminjul <yulya.demina.01@gmail.com>
2025-05-20 11:05:46 +0300

    о
    о

--- herzenbooks/src/components/UI/Footer.tsx
+++ herzenbooks/src/components/UI/Footer.tsx
@@ -5,16 +5,16 @@ export const Footer =()=> {
     
     return (
         
-        <footer className="flex min-h-70 mt-40 bg-[rgb(43,64,143)]" id="contacts">
+        <footer className="flex  min-h-70 mt-40 bg-[rgb(43,64,143)]" id="contacts">
 
-        <div className='flex mx-auto w-2/3  mt-20 justify-center gap-20 font-bold text-white' style={{ fontFamily: 'Akrobat' }}>
+        <div className='flex flex-wrap mx-auto w-5/6 mt-20 justify-start gap-20 font-bold text-white' style={{ fontFamily: 'Akrobat' }}>
             <>
-            <button
+            <button className="flex w-60"
                 onClick={() => navigate ("/")}>
                     <img src="logo2.svg" alt="LoGO" className="h-10"/>
             </button>
-            <div className="flex flex-col w-60"> 
-                <h1 className="text-2xl">контакты</h1>
+            <div className="flex flex-col w-57"> 
+                <h1 className="text-2xl mb-5">контакты</h1>
                 <p> 8(812)570-59-89</p>
                 <a className="underline" href="https://vk.com/herzenbookshop"
                 target="_blank" // открывает ссылку в новой вкладке
@@ -28,7 +28,7 @@ export const Footer =()=> {
             </div>
 
             <div className="flex flex-col w-60"> 
-                <h1 className="font-bold text-2xl">адрес</h1>
+                <h1 className="font-bold text-2xl mb-5">адрес</h1>
                 <p> Казанская улица, 1/25, Санкт-Петербург</p>
 
                 <a className="underline" href="https://yandex.ru/maps/org/herzenknigi/241933995431/?ll=30.323464%2C59.934904&z=14.15"
@@ -44,7 +44,7 @@ export const Footer =()=> {
             </div>
 
             <div className="flex flex-col w-60"> 
-                <h1 className=" text-2xl" >график</h1>
+                <h1 className=" text-2xl mb-5" >график работы</h1>
                 <p>Ежедневно с 10 до 21:00</p>
                
             </div>
